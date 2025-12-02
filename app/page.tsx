58834ca2df0182f90265d4d6f72b176c3b3a38fe
Deepanshu-Z <deepanshupokhriyal07@gmail.com>
2025-12-02 11:47:46 +0530

    Components: {Products, Hero}

--- app/page.tsx
+++ app/page.tsx
@@ -3,12 +3,15 @@ import SingleProduct from "@/components/shopping/SingleProduct";
 import Testimonial from "@/components/landing/Testimonial";
 import Image from "next/image";
 import LandingProduct from "@/components/landing/LandingProduct";
+import Products from "@/components/landing/Products";
+import Integrations from "@/components/landing/Integrations";
 
 export default function Home() {
   return (
     <div>
       <Hero />
-      <LandingProduct />
+      <Integrations />
+      <Products />
       <Testimonial />
     </div>
   );
