8b20b517c94a52d3da91e726118511ca7e399fd4
Deepanshu-Z <deepanshupokhriyal07@gmail.com>
2025-12-02 11:41:10 +0530

    Updated {Hero section}

--- components/landing/Hero.tsx
+++ components/landing/Hero.tsx
@@ -1,176 +1,132 @@
-import { Mail, SendHorizonal } from "lucide-react";
+import { ArrowDownRight, Star } from "lucide-react";
+
+import { Avatar, AvatarImage } from "@/components/ui/avatar";
 import { Button } from "@/components/ui/button";
-import { TextEffect } from "@/components/ui/text-effect";
-import { AnimatedGroup } from "@/components/ui/animated-group";
-import { Logo } from "@/components/landing/logo";
 
-const transitionVariants = {
-  item: {
-    hidden: {
-      opacity: 0,
-      filter: "blur(12px)",
-      y: 12,
-    },
-    visible: {
-      opacity: 1,
-      filter: "blur(0px)",
-      y: 0,
-      transition: {
-        type: "spring",
-        bounce: 0.3,
-        duration: 1.5,
-      },
-    },
-  },
-};
-
-export default function Hero() {
-  return (
-    <>
-      <main className="overflow-hidden [--color-primary-foreground:var(--color-white)] [--color-primary:var(--color-green-600)]">
-        <section>
-          <div className="relative mx-auto max-w-6xl px-6 pb-20 pt-32 lg:pt-48">
-            <div className="relative z-10 mx-auto max-w-4xl text-center">
-              <TextEffect
-                preset="fade-in-blur"
-                speedSegment={0.3}
-                as="h1"
-                className="text-balance text-5xl font-medium md:text-6xl"
-              >
-                Nutrivya
-              </TextEffect>
-              <TextEffect
-                per="line"
-                preset="fade-in-blur"
-                speedSegment={0.3}
-                delay={0.5}
-                as="p"
-                className="mx-auto mt-6 max-w-2xl text-pretty text-lg"
-              >
-                An Ayurvedic approach crafted to naturally support fat-control
-                and lean muscle development. Contains traditional herbs known
-                for helping balance metabolism and promoting overall strength.
-              </TextEffect>
-
-              <AnimatedGroup
-                //@ts-ignore
-                variants={{
-                  container: {
-                    visible: {
-                      transition: {
-                        staggerChildren: 0.05,
-                        delayChildren: 0.75,
-                      },
-                    },
-                  },
-                  ...transitionVariants,
-                }}
-                className="mt-12"
-              >
-                <form action="" className="mx-auto max-w-sm">
-                  <div className="bg-background has-[input:focus]:ring-muted relative grid grid-cols-[1fr_auto] items-center rounded-[calc(var(--radius)+0.5rem)] border pr-2 shadow shadow-zinc-950/5 has-[input:focus]:ring-2">
-                    <Mail className="pointer-events-none absolute inset-y-0 left-4 my-auto size-4" />
-
-                    <input
-                      placeholder="Your mail address"
-                      className="h-12 w-full bg-transparent pl-12 focus:outline-none"
-                      type="email"
-                    />
-
-                    <div className="md:pr-1.5 lg:pr-0">
-                      <Button
-                        aria-label="submit"
-                        size="sm"
-                        className="rounded-(--radius)"
-                      >
-                        <span className="hidden md:block">Get Started</span>
-                        <SendHorizonal
-                          className="relative mx-auto size-5 md:hidden"
-                          strokeWidth={2}
-                        />
-                      </Button>
-                    </div>
-                  </div>
-                </form>
-
-                <div
-                  aria-hidden
-                  className="bg-radial from-primary/50 dark:from-primary/25 relative mx-auto mt-32 max-w-2xl to-transparent to-55% text-left"
-                >
-                  <div className="bg-background border-border/50 absolute inset-0 mx-auto w-80 -translate-x-3 -translate-y-12 rounded-[2rem] border p-2 [mask-image:linear-gradient(to_bottom,#000_50%,transparent_90%)] sm:-translate-x-6">
-                    <div className="relative h-96 overflow-hidden rounded-[1.5rem] border p-2 pb-12 before:absolute before:inset-0 before:bg-[repeating-linear-gradient(-45deg,var(--color-border),var(--color-border)_1px,transparent_1px,transparent_6px)] before:opacity-50"></div>
-                  </div>
-                  <div className="bg-muted dark:bg-background/50 border-border/50 mx-auto w-80 translate-x-4 rounded-[2rem] border p-2 backdrop-blur-3xl [mask-image:linear-gradient(to_bottom,#000_50%,transparent_90%)] sm:translate-x-8">
-                    <div className="bg-background space-y-2 overflow-hidden rounded-[1.5rem] border p-2 shadow-xl dark:bg-white/5 dark:shadow-black dark:backdrop-blur-3xl">
-                      <AppComponent />
-
-                      <div className="bg-muted rounded-[1rem] p-4 pb-16 dark:bg-white/5"></div>
-                    </div>
-                  </div>
-                  <div className="absolute inset-0 bg-[radial-gradient(#e5e7eb_1px,transparent_1px)] mix-blend-overlay [background-size:16px_16px] [mask-image:radial-gradient(ellipse_50%_50%_at_50%_50%,#000_70%,transparent_100%)] dark:opacity-5"></div>
-                </div>
-              </AnimatedGroup>
-            </div>
-          </div>
-        </section>
-      </main>
-    </>
-  );
+interface Hero3Props {
+  heading?: string;
+  description?: string;
+  buttons?: {
+    primary?: {
+      text: string;
+      url: string;
+    };
+    secondary?: {
+      text: string;
+      url: string;
+    };
+  };
+  reviews?: {
+    count: number;
+    avatars: {
+      src: string;
+      alt: string;
+    }[];
+    rating?: number;
+  };
 }
 
-const AppComponent = () => {
+const Hero = ({
+  heading = "Blocks built with Shadcn & Tailwind",
+  description = "Finely crafted components built with React, Tailwind and Shadcn UI. Developers can copy and paste these blocks directly into their project.",
+  buttons = {
+    primary: {
+      text: "Sign Up",
+      url: "https://www.shadcnblocks.com",
+    },
+    secondary: {
+      text: "Get Started",
+      url: "https://www.shadcnblocks.com",
+    },
+  },
+  reviews = {
+    count: 200,
+    rating: 5.0,
+    avatars: [
+      {
+        src: "https://deifkwefumgah.cloudfront.net/shadcnblocks/block/avatar-1.webp",
+        alt: "Avatar 1",
+      },
+      {
+        src: "https://deifkwefumgah.cloudfront.net/shadcnblocks/block/avatar-2.webp",
+        alt: "Avatar 2",
+      },
+      {
+        src: "https://deifkwefumgah.cloudfront.net/shadcnblocks/block/avatar-3.webp",
+        alt: "Avatar 3",
+      },
+      {
+        src: "https://deifkwefumgah.cloudfront.net/shadcnblocks/block/avatar-4.webp",
+        alt: "Avatar 4",
+      },
+      {
+        src: "https://deifkwefumgah.cloudfront.net/shadcnblocks/block/avatar-5.webp",
+        alt: "Avatar 5",
+      },
+    ],
+  },
+}: Hero3Props) => {
   return (
-    <div className="relative space-y-3 rounded-[1rem] bg-white/5 p-4">
-      <div className="flex items-center gap-1.5 text-orange-400">
-        <svg
-          className="size-5"
-          xmlns="http://www.w3.org/2000/svg"
-          width="1em"
-          height="1em"
-          viewBox="0 0 32 32"
-        >
-          <g fill="none">
-            <path
-              fill="#ff6723"
-              d="M26 19.34c0 6.1-5.05 11.005-11.15 10.641c-6.269-.374-10.56-6.403-9.752-12.705c.489-3.833 2.286-7.12 4.242-9.67c.34-.445.689 3.136 1.038 2.742c.35-.405 3.594-6.019 4.722-7.991a.694.694 0 0 1 1.028-.213C18.394 3.854 26 10.277 26 19.34"
-            ></path>
-            <path
-              fill="#ffb02e"
-              d="M23 21.851c0 4.042-3.519 7.291-7.799 7.144c-4.62-.156-7.788-4.384-7.11-8.739C9.07 14.012 15.48 10 15.48 10S23 14.707 23 21.851"
-            ></path>
-          </g>
-        </svg>
-        <div className="text-sm font-medium">Steps</div>
-      </div>
-      <div className="space-y-3">
-        <div className="text-foreground border-b border-white/10 pb-3 text-sm font-medium">
-          This year, you're walking more on average than you did in 2023.
-        </div>
-        <div className="space-y-3">
-          <div className="space-y-1">
-            <div className="space-x-1">
-              <span className="text-foreground align-baseline text-xl font-medium">
-                8,081
-              </span>
-              <span className="text-muted-foreground text-xs">Steps/day</span>
-            </div>
-            <div className="flex h-5 items-center rounded bg-gradient-to-l from-emerald-400 to-indigo-600 px-2 text-xs text-white">
-              2024
+    <section className="p-32">
+      <div className="container grid items-center gap-10 lg:grid-cols-2 lg:gap-20">
+        <div className="mx-auto flex flex-col items-center text-center md:ml-auto lg:max-w-3xl lg:items-start lg:text-left">
+          <h1 className="my-6 text-pretty text-4xl font-bold lg:text-6xl xl:text-7xl">
+            {heading}
+          </h1>
+          <p className="text-muted-foreground mb-8 max-w-xl lg:text-xl">
+            {description}
+          </p>
+          <div className="mb-12 flex w-fit flex-col items-center gap-4 sm:flex-row">
+            <span className="inline-flex items-center -space-x-4">
+              {reviews.avatars.map((avatar, index) => (
+                <Avatar key={index} className="size-12 border">
+                  <AvatarImage src={avatar.src} alt={avatar.alt} />
+                </Avatar>
+              ))}
+            </span>
+            <div>
+              <div className="flex items-center gap-1">
+                {[...Array(5)].map((_, index) => (
+                  <Star
+                    key={index}
+                    className="size-5 fill-yellow-400 text-yellow-400"
+                  />
+                ))}
+                <span className="mr-1 font-semibold">
+                  {reviews.rating?.toFixed(1)}
+                </span>
+              </div>
+              <p className="text-muted-foreground text-left font-medium">
+                from {reviews.count}+ reviews
+              </p>
             </div>
           </div>
-          <div className="space-y-1">
-            <div className="space-x-1">
-              <span className="text-foreground align-baseline text-xl font-medium">
-                5,412
-              </span>
-              <span className="text-muted-foreground text-xs">Steps/day</span>
-            </div>
-            <div className="text-foreground bg-muted flex h-5 w-2/3 items-center rounded px-2 text-xs dark:bg-white/20">
-              2023
-            </div>
+          <div className="flex w-full flex-col justify-center gap-2 sm:flex-row lg:justify-start">
+            {buttons.primary && (
+              <Button asChild className="w-full sm:w-auto">
+                <a href={buttons.primary.url}>{buttons.primary.text}</a>
+              </Button>
+            )}
+            {buttons.secondary && (
+              <Button asChild variant="outline">
+                <a href={buttons.secondary.url}>
+                  {buttons.secondary.text}
+                  <ArrowDownRight className="size-4" />
+                </a>
+              </Button>
+            )}
           </div>
         </div>
+        <div className="flex">
+          <img
+            src="https://deifkwefumgah.cloudfront.net/shadcnblocks/block/placeholder-1.svg"
+            alt="placeholder hero"
+            className="max-h-[600px] w-full rounded-md object-cover lg:max-h-[800px]"
+          />
+        </div>
       </div>
-    </div>
+    </section>
   );
 };
+
+export default Hero;
